970d5e08e817a9e4f97cda8942810ba3d54419c3
MelhaouiTarek <Melhaoui.Tarek@gmail.com>
2021-11-17 20:18:03 +0100

    Ajout du TP2

---
+++ src/main/java/ma/emsi/tp_prg_jsf_melhaouitarek/config/ConfigJSF.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ma.emsi.tp_prg_jsf_melhaouitarek.config;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author Xune
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
---
+++ src/main/java/ma/emsi/tp_prg_jsf_melhaouitarek/jsf/NewBean.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ma.emsi.tp_prg_jsf_melhaouitarek.jsf;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+@Named(value = "newbean")
+@RequestScoped
+public class NewBean {
+
+    private int nombre;
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+    public String afficher() {
+    return "affichage_4?nb="+ nombre + "&amp;faces-redirect=true";
+}
+    public String goBack()
+    {
+         return "formulaire_4?nb="+ nombre + "&amp;faces-redirect=true";
+    }
+}
